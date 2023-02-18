b2633ab93607d4f37a48b11ff088a29225d0cf1e
Elie Bissor <eliebiss.26@gmail.com>
2023-02-18 23:59:18 +0100

    Add images.

---
+++ src/assets/MarioImages.ts
@@ -0,0 +1,21 @@
+import Image1 from "./images/super-mario/mario-character-1.png";
+import Image2 from "./images/super-mario/mario-character-2.png";
+import Image3 from "./images/super-mario/mario-character-3.png";
+import Image4 from "./images/super-mario/mario-character-4.png";
+import Image5 from "./images/super-mario/mario-character-5.png";
+import Image6 from "./images/super-mario/mario-character-6.png";
+import Image7 from "./images/super-mario/mario-character-7.png";
+import Image8 from "./images/super-mario/mario-character-8.png";
+
+const MarioImages = [
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+];
+
+export default MarioImages;
